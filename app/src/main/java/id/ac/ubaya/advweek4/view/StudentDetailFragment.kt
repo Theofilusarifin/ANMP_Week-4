1ad2ef5d0b7f72876ee5e1e211fb12ff6b1a156b
Theofilusarifin <theofilusarifin@gmail.com>
2023-03-28 17:32:54 +0700

    Fix Error Week 5

--- app/src/main/java/id/ac/ubaya/advweek4/view/StudentDetailFragment.kt
+++ app/src/main/java/id/ac/ubaya/advweek4/view/StudentDetailFragment.kt
@@ -5,58 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import id.ac.ubaya.advweek4.R
-//import id.ac.ubaya.advweek4.databinding.FragmentStudentDetailBinding
-import id.ac.ubaya.advweek4.viewmodel.DetailViewModel
-import kotlinx.android.synthetic.main.fragment_student_detail.*
 
-class StudentDetailFragment : Fragment(),
-    ButtonUpdateClickListener {
+class StudentDetailFragment : Fragment() {
 
-    private lateinit var viewDetail: DetailViewModel
-//    private lateinit var dataBinding: FragmentStudentDetailBinding
-
-//    override fun onCreateView(
-//        inflater: LayoutInflater, container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View? {
-////        dataBinding = DataBindingUtil.inflate<FragmentStudentDetailBinding>(
-////            inflater, R.layout.fragment_student_detail, container, false)
-////
-////        return dataBinding.root
-//    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        if(arguments != null) {
-            val studentId = StudentDetailFragmentArgs.fromBundle(requireArguments()).studentId
-
-            viewDetail = ViewModelProvider(this).get(DetailViewModel::class.java)
-            viewDetail.fetch()
-
-            observeDetailViewModel()
-        }
-    }
-
-    fun observeDetailViewModel() {
-        viewDetail.studentLD.observe(viewLifecycleOwner, Observer {
-//            dataBinding.student = it
-
-            txtIdDetail.setText(it.id)
-            txtNameDetail.setText(it.name)
-            txtBod.setText(it.dob)
-            txtPhone.setText(it.phone)
-//
-//            imageView2.loadImage(it.photoUrl.toString(), progressBar2)
-        })
-    }
-
-    override fun onButtonUpdateClick(v: View) {
-        Toast.makeText(v.context, "Update Success", Toast.LENGTH_SHORT).show()
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_student_detail, container, false)
     }
 }
--- app/src/main/java/id/ac/ubaya/advweek4/view/StudentListFragment.kt
+++ app/src/main/java/id/ac/ubaya/advweek4/view/StudentListFragment.kt
@@ -5,16 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.Observer
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import id.ac.ubaya.advweek4.R
 import id.ac.ubaya.advweek4.viewmodel.ListViewModel
 import kotlinx.android.synthetic.main.fragment_student_list.*
 
 class StudentListFragment : Fragment() {
+
     private lateinit var viewModel: ListViewModel
-    private val studentListAdapter  = StudentListAdapter(arrayListOf())
+    private val studentListAdapter = StudentListAdapter(arrayListOf())
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,34 +30,51 @@ class StudentListFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
         viewModel = ViewModelProvider(this).get(ListViewModel::class.java)
         viewModel.refresh()
 
-        recView.layoutManager = LinearLayoutManager(context)
-        recView.adapter = studentListAdapter
+        val recView = view.findViewById<RecyclerView>(R.id.recView)
+        recView?.layoutManager = LinearLayoutManager(context)
+        recView?.adapter = studentListAdapter
 
         observeViewModel()
+
+        val swipe = view.findViewById<SwipeRefreshLayout>(R.id.refreshLayout)
+        swipe.setOnRefreshListener {
+            recView.visibility = View.GONE
+            txtError.visibility = View.GONE
+            progressLoad.visibility = View.VISIBLE
+            viewModel.refresh()
+            refreshLayout.isRefreshing = false
+        }
     }
 
-    fun observeViewModel() {
-        viewModel.studentsLD.observe(viewLifecycleOwner, Observer {
+    private fun observeViewModel() {
+        viewModel.studentsLD.observe(viewLifecycleOwner){
             studentListAdapter.updateStudentList(it)
-        })
+        }
+        viewModel.studentLoadErrorLD.observe(viewLifecycleOwner){
+            val txtError = view?.findViewById<TextView>(R.id.txtError)
 
-        viewModel.studentLoadErrorLD.observe(viewLifecycleOwner, Observer {
-            txtError.visibility = if (it) View.VISIBLE else View.GONE
-        })
+            txtError?.visibility = if(it) View.VISIBLE else View.GONE
 
-        viewModel.loadingLD.observe(viewLifecycleOwner, Observer {
-            if (it) {
-                progressLoad.visibility = View.VISIBLE
-                recView.visibility = View.GONE
-            } else {
-                progressLoad.visibility = View.GONE
-                recView.visibility = View.VISIBLE
+            viewModel.loadingLD.observe(viewLifecycleOwner){
+                val recView = view?.findViewById<RecyclerView>(R.id.recView)
+                val progressLoad = view?.findViewById<ProgressBar>(R.id.progressLoad)
+
+                if(it){ //sedang loading
+                    recView?.visibility = View.GONE
+                    progressLoad?.visibility = View.VISIBLE
+                    txtError?.visibility = View.VISIBLE
+                }else{
+                    recView?.visibility = View.VISIBLE
+                    progressLoad?.visibility = View.GONE
+                    txtError?.visibility = View.GONE
+
+                }
             }
-        })
+
+        }
     }
+
 }
